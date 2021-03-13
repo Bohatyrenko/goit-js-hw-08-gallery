b39c03bd9e8147eafd64cfe3aeeb2ad7218b0f52
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-03-14 00:19:06 +0200

    Добавляет код галереи

--- js/task.js
+++ js/task.js
@@ -1,60 +1,82 @@
 import gallery from "./gallery-items.js";
 
-// console.log(makeItemsGallery(gallery));
+const galleryListRef = document.querySelector(".js-gallery");
+const modalRef = document.querySelector(".js-lightbox");
+const imgModalRef = document.querySelector(".lightbox__image");
+const closeModalBtnRef = document.querySelector(".lightbox__button");
+const lightboxOverlay = document.querySelector(".lightbox__overlay");
 
-const galleryList = document.querySelector(".js-gallery");
-const openModalRef = document.querySelector(".lightbox");
-const imageInModal = document.querySelector(".lightbox__image");
-const closeModalBtn = document.querySelector(".lightbox__button");
-const itemImage = document.querySelector(".gallery__image");
-const itemLink = document.querySelector(".gallery__link");
+/**Переменная индекса картинки*/
+let activeIndex = 0;
 
-/**Функция создания разметки галереи */
-function makeItemsGallery(gallery) {
-  return gallery
-    .map(({ preview, original, description }) => {
-      return `
-    <li class="gallery__item">
-      <a
-        class="gallery__link"
-        href="${original}"
-      >
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>
-  `;
-    })
-    .join("");
-}
+/**Создаем гроздь галереи */
+const makeGallery = gallery.map(({ preview, original, description }) => {
+  return `
+        <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+});
 
-function onItemGalleryClick(e) {
-  if (!e.target.classList(".gallery__image")) {
-    return;
+/**Вешаем гроздь в разметку, join - связывает массив строк в одну строку*/
+galleryListRef.insertAdjacentHTML("beforeend", makeGallery.join(""));
+
+/**Функция открытия модалки через добавление класса is-open */
+function openModalFn(e) {
+  e.preventDefault();
+  if (e.target.nodeName === "IMG") {
+    modalRef.classList.add("is-open");
+    imgModalRef.src = e.target.dataset.source;
   }
 }
 
-function openModalFn() {
-  openModalRef.classList.add(".lightbox.is-open");
-}
+/**Добавляет слушатель события на ul (только на IMG) для открытия модалки */
+galleryListRef.addEventListener("click", openModalFn);
 
-function onDefaultLink(e) {
-  e.preventDefault();
+/**Функция закрытия модального окна, снятия класса is-open и очистку SRC */
+function closeModal() {
+  modalRef.classList.remove("is-open");
+  imgModalRef.removeAttribute("src");
 }
+/**Добавляет слушатель события на закрытие модалки через нажатие на overlay */
+lightboxOverlay.addEventListener("click", closeModal);
 
-function imageInModalFn({ src, alt }) {
-  imageInModal.src = `${src}`;
-  imageInModal.alt = `${alt}`;
+/**Добавляет слушатель события на закрытие модалки через нажатие на кнопку */
+closeModalBtnRef.addEventListener("click", closeModal);
+
+/**Аналог функции закрытия модального окна через кнопку (Снимает класс is-open и убирает значение из SRC */
+// closeModalBtnRef.onclick = () => {
+//   modalRef.classList.remove("is-open");
+//   imgModalRef.removeAttribute("src");
+// };
+
+/**Закрытие модального окна через клавишу Escape */
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+});
+
+/**Функция "карусели" перелистывание картинок кнопками ArrowLeft и ArrowRight*/
+function onPressArrow(e) {
+  if (e.key === "ArrowLeft" && activeIndex > 0) {
+    activeIndex -= 1;
+    imgModalRef.src = gallery[activeIndex].original;
+  }
+  if (e.key === "ArrowRight" && activeIndex < gallery.length - 1) {
+    activeIndex += 1;
+    imgModalRef.src = gallery[activeIndex].original;
+  }
 }
-/**Вешает гроздь в разметку index */
-const makeCardsGallery = makeItemsGallery(gallery);
-galleryList.insertAdjacentHTML("beforeend", makeCardsGallery);
-
-/**Слушатели событий */
-galleryList.addEventListener("click", onItemGalleryClick);
-openModalRef.addEventListener("click", openModalFn);
-itemLink.addEventListener("click", onDefaultLink);
+/**Добавляет слушатель события кнопки ArrowLeft и ArrowRightна для перелистывание картинок*/
+window.addEventListener("keydown", onPressArrow);
